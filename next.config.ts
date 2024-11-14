b87b4c35c83311cdb6e2326343503cb27ea900ea
sergiogarciauribe <sergio.341822090@ucaldas.edu.co>
2024-11-14 17:01:48 -0500

    prueba 3

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
-import type { NextConfig } from 'next';
+import type { NextConfig } from "next";
+
+//const nextConfig: NextConfig = {
+/* config options here */
+//};
 
 const nextConfig: NextConfig = {
   /* config options here */
+  // experimental: {
+  //   ppr: "incremental",
+  // },
 };
 
 export default nextConfig;
